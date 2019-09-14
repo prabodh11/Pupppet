8355a23f8ef85ad11aa88b53be3e86d1cbacdb53
prabodh11 <prabodh.swain11@gmail.com>
2019-09-14 21:33:11 +0530

    fixed syntax error

--- manifests/file_hello.pp
+++ manifests/file_hello.pp
@@ -1,7 +1,7 @@
 file {'/tmp/hello.txt':
   ensure	=> "file",
-  content   => "Hello, World\n",
+  content   	=> "Hello, World\n",
   owner 	=> "demouser",
   group 	=> "demouser",
-  mode 		=> "0755"
+  mode 		=> "0755",
 }
