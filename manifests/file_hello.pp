22fd0035791a1b791e50c5ac51168e04a973e8d5
prabodh11 <prabodh.swain11@gmail.com>
2019-09-14 21:29:24 +0530

    modified file_hello.pp

--- manifests/file_hello.pp
+++ manifests/file_hello.pp
@@ -1,4 +1,7 @@
 file {'/tmp/hello.txt':
-  ensure        => "file",
-  content       => "Hello, World\n",
+  ensure	=> "file",
+  content   => "Hello, World\n",
+  owner 	=> "demouser",
+  group 	=> "demouser",
+  mode 		=> "0755"
 }
